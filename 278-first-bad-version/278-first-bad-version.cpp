f5e266144a8a4e0ff05adc71b2bd486d53b8d363
Parkhi Garg <76432727+PARKHI277@users.noreply.github.com>
2022-08-26 13:18:41 +0530

    Time: 6 ms (7.34%), Space: 5.8 MB (67.89%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -5,17 +5,21 @@ class Solution {
 public:
     int firstBadVersion(int n)
     {
-         int start=0, end=n;
-        cout<<end-start<<end;
-        while(end-start>1)
-        {
-            int mid=start+(end-start)/2;
-          
-            if(isBadVersion(mid))  
-                end=mid;
-            else  
-                start=mid;
+        int s = 0;
+        int e = n;
+        int bad;
+        while(s<=e){
+            
+            int mid = s + (e-s)/2;
+            
+            if(isBadVersion(mid)){
+                bad = mid;
+                e = mid-1;
+            }
+            else
+                s = mid+1;
+            
         }
-        return end;
+        return bad;
     }
 };
