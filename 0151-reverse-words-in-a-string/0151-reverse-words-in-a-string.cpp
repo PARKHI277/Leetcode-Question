a86656c0579ff6ada3d0ea60e85567ffb5002d33
Parkhi Garg <76432727+PARKHI277@users.noreply.github.com>
2022-11-13 09:55:35 +0530

    Time: 29 ms (15.15%), Space: 20.8 MB (29.74%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+     string tmp="", ans="";
+        for(int i=0;i<s.size();i++)
+        {
+            char ch = s[i];
+            if(ch==' ')
+            {
+                if(tmp!="")
+                    ans = tmp + " " + ans;
+                tmp = "";        
+            }
+            else 
+                tmp += ch;
+        }
+        
+        if(tmp!="")
+            ans = tmp + " " + ans;
+        ans.pop_back();
+        
+        return ans;      
+    }
+        
+    
+};
+
